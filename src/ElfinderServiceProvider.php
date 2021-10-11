d8e97f872c7414f88cfc710c31236f5fded36558
Peter Kazakov <kazak.71@gmail.com>
2021-10-11 22:23:50 +0300

    Update ElfinderServiceProvider.php

--- src/ElfinderServiceProvider.php
+++ src/ElfinderServiceProvider.php
@@ -3,42 +3,42 @@
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
-class ElfinderServiceProvider extends ServiceProvider {
+class ElfinderServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $configPath = __DIR__ . '/../config/elfinder.php';
         $this->mergeConfigFrom($configPath, 'elfinder');
         $this->publishes([$configPath => config_path('elfinder.php')], 'config');
 
-        $this->app->singleton('command.elfinder.publish', function($app)
-        {
-			$publicPath = $app['path.public'];
+        $this->app->singleton('command.elfinder.publish', function ($app) {
+            $publicPath = $app['path.public'];
             return new Console\PublishCommand($app['files'], $publicPath);
         });
         $this->commands('command.elfinder.publish');
-	}
+    }
 
-	/**
-	 * Define your route model bindings, pattern filters, etc.
-	 *
-	 * @param  \Illuminate\Routing\Router  $router
-	 * @return void
-	 */
-	public function boot(Router $router)
-	{
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function boot(Router $router)
+    {
         $viewPath = __DIR__.'/../resources/views';
         $this->loadViewsFrom($viewPath, 'elfinder');
         $this->publishes([
@@ -46,15 +46,14 @@ class ElfinderServiceProvider extends ServiceProvider {
         ], 'views');
 
         if (!defined('ELFINDER_IMG_PARENT_URL')) {
-			define('ELFINDER_IMG_PARENT_URL', $this->app['url']->asset('packages/barryvdh/elfinder'));
-		}
+            define('ELFINDER_IMG_PARENT_URL', $this->app['url']->asset('packages/kazak71/elfinder'));
+        }
 
         $config = $this->app['config']->get('elfinder.route', []);
         $config['namespace'] = 'Barryvdh\Elfinder';
 
-        $router->group($config, function($router)
-        {
-            $router->get('/',  ['as' => 'elfinder.index', 'uses' =>'ElfinderController@showIndex']);
+        $router->group($config, function ($router) {
+            $router->get('/', ['as' => 'elfinder.index', 'uses' =>'ElfinderController@showIndex']);
             $router->any('connector', ['as' => 'elfinder.connector', 'uses' => 'ElfinderController@showConnector']);
             $router->get('popup/{input_id}', ['as' => 'elfinder.popup', 'uses' => 'ElfinderController@showPopup']);
             $router->get('filepicker/{input_id}', ['as' => 'elfinder.filepicker', 'uses' => 'ElfinderController@showFilePicker']);
@@ -63,16 +62,15 @@ class ElfinderServiceProvider extends ServiceProvider {
             $router->get('tinymce5', ['as' => 'elfinder.tinymce5', 'uses' => 'ElfinderController@showTinyMCE5']);
             $router->get('ckeditor', ['as' => 'elfinder.ckeditor', 'uses' => 'ElfinderController@showCKeditor4']);
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('command.elfinder.publish');
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('command.elfinder.publish');
+    }
 }
